44ab379f43569b4cc951356e895cdaa62e2d5d1c
LuisToro-v <Vidals824@yahoo.com>
2021-04-09 14:53:14 -0400

    Create food table (#25)
    * Added food table
    
    * Added seed data for food
    
    * Altered user_seed and tested seed

--- seeds/user_seed.js
+++ seeds/user_seed.js
@@ -2,9 +2,12 @@ const userData = require('../seed_data/user')
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('users').del()
+  return knex('food').del()
     .then(() => {
+      return knex('users').del()
+      .then(() => {
       // Inserts seed entries
       return knex('users').insert(userData);
     });
+  });
 };
