46306ec3bf0da32b4e7dd39337e4d9392bc15a0c
e-mbrown <ebsonari@gmail.com>
2021-04-09 15:02:00 -0400

    Set up pgPromise connection and test, Started user routes and set up for test

---
+++ db/db.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const pgp = require("pg-promise")()
+
+const cn = {
+    host: 'localhost',
+    //default  port
+    port: 5432,
+    database: 'footprint_test',
+    user: process.env.USER,
+    password: process.env.PASSWORD
+}
+
+const db = pgp(cn)
+
+db.connect()
+    .then(obj => {
+        //saves connection object
+        sco = obj
+        return sco.one('SELECT * FROM users LIMIT 1')
+    })
+    .then(data =>{
+        console.log(data.id, 'connection tested')
+    })
+    .catch( err => {
+        console.log(`There is an issue with the db. Error: ${err}`)
+    })
+    .finally(() =>{
+        if(sco){
+            console.log('Db fully operational')
+            sco.done()
+        }
+    })
+module.exports = db
